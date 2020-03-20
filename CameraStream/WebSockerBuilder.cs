f8df9205d8bb10b823b76d345a45750d595784e7
Arda Cetinkaya <ardacetinkaya@gmail.com>
2020-03-20 21:25:40 +0300

    Init

---
+++ CameraStream/WebSockerBuilder.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CameraStream
+{
+    public static class WebSocketBuilder
+    {
+        public static IApplicationBuilder UseStreamSocket(this IApplicationBuilder app)
+        {
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Stream(context, webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+
+
+
+            return app;
+        }
+
+        private static async Task Stream(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                ArraySegment<byte> streamData = new ArraySegment<byte>(buffer, 0, result.Count);
+                await webSocket.SendAsync(streamData, result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                var outputData = new ArraySegment<byte>(buffer);
+
+                result = await webSocket.ReceiveAsync(outputData, CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
